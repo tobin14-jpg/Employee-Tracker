b127ce036c063ae3945274b4aeec6dd20c749f73
Ice Tea <shauntobin88@gmail.com>
2021-08-30 18:33:55 +0800

    added gif & final touches

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO department (name)
+INSERT INTO department (id, name)
 VALUES
 (001, "Finance & Accounting"),
 (002, "Marketing"),
@@ -10,7 +10,7 @@ VALUES
 (001, "Financial Manager", 120000, 001),
 (002, "Marketing Manager", 100000, 002),
 (003, "Lead Engineer", 150000, 003),
-(004, "Sales Manager", 90000, 004);
+(004, "Sales Manager", 90000, 004),
 (005, "Finance Analyst", 110000, 001),
 (006, "Marketing Analyst", 850000, 002),
 (007, "Software Engineer", 120000, 003),
@@ -21,7 +21,7 @@ VALUES
 (001, "Wayne", "Gretzky", 001, NULL),
 (002, "Michael", "Jordan", 002, NULL),
 (003, "Steve", "Jobs", 003, NULL),
-(004, "Tony", "Stark", 004, NULL);
+(004, "Tony", "Stark", 004, NULL),
 (005, "Sidney", "Crosby", 005, 001),
 (006, "Lebron", "James", 006, 002),
 (007, "Elon", "Musk", 007, 003),
